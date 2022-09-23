da828ed6325fb754da2c4533ca1420376d692b7f
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-09-23 14:50:25 -0700

    Initial Commit

---
+++ PerfectDog/PerfectDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectDogApp.swift
+//  PerfectDog
+//
+//  Created by Jong Hwan Seo on 2022-09-23.
+//
+
+import SwiftUI
+
+@main
+struct PerfectDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
