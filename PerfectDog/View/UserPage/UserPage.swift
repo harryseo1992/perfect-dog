c0e00b619dd92b81e2f67d2b7a30275eaabb2ce2
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-10-14 14:49:50 -0700

    Updated assets and pages according to what we learned in 4977

--- PerfectDog/View/UserPage/UserPage.swift
+++ PerfectDog/View/UserPage/UserPage.swift
@@ -7,111 +7,55 @@
 
 import SwiftUI
 
+let coloredNavAppearance = UINavigationBarAppearance()
+
 struct UserPage: View {
-    @State var selectedTab = Tabs.FirstTab
-    
+    init() {
+//            coloredNavAppearance.configureWithOpaqueBackground()
+        coloredNavAppearance.backgroundColor = .perfectDog
+            coloredNavAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            coloredNavAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+                   
+            UINavigationBar.appearance().standardAppearance = coloredNavAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
+
+        }
     var body: some View {
         VStack {
-            Group { // Tabs
-                VStack{
-                    HStack {
-                        Text("Hello, User!")
-                            .font(.system(size: 30)).bold()
-                        Spacer()
-                        Image("doggy")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                            .clipped()
-                    }
-                    .padding([.top, .leading, .trailing])
-                    .background(Color.perfectDog.edgesIgnoringSafeArea(.all))
-                HStack {
-                    Spacer()
-                    VStack {
-                        Image(systemName: "gamecontroller.fill")
-                            .foregroundColor(selectedTab == .FirstTab ? Color.white : Color.black)
-                        Text("Play")
-                            .padding([.top, .bottom], 5)
-                    }
-                    .onTapGesture {
-                        self.selectedTab = .FirstTab
-                    }
-                    Spacer()
-                    VStack {
-                        Image(systemName: "person.fill")
-                            .foregroundColor(selectedTab == .SecondTab ? Color.white : Color.black)
-                        Text("Past History")
-                            .padding([.top, .bottom], 5)
-                    }
-                    .onTapGesture {
-                        self.selectedTab = .SecondTab
-                    }
-                    Spacer()
-                }
-                }
-                .background(Color.perfectDog.edgesIgnoringSafeArea(.all))
-            }
-            VStack {
-                Image("PerfectDog")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .padding(.top, 50)
-                Text("Perfect Doggo")
-                Spacer()
-                Button(action: {}) {
-                    Text("Choose your perfect doggo")
-                        .fontWeight(.medium)
-                        .padding(8.0)
-                }
-                    .foregroundColor(.black)
-                    .frame(maxWidth:.infinity)
-                    .background(Color.perfectDog)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 100)
-                            .stroke(Color.perfectDog, lineWidth: 1)
-                    )
-                    .padding([.leading, .trailing, .bottom])
-            }
-            Spacer()
-            
-            if selectedTab == .FirstTab {
-                FirstTabView()
-            } else if selectedTab == .SecondTab {
-                SecondTabView()
+            NavigationView {
+                TabView {
+                    PerfectDogTabViewItem()
+                    PastHistoryTabViewItem()
+                }.navigationTitle("Hello, User!")
+                    .accentColor(Color.perfectDog)
             }
         }
     }
 }
 
-struct FirstTabView : View {
-    
-    var body : some View {
-        VStack {
-            Text("FIRST TAB VIEW")
-        }
-    }
-}
-
-struct SecondTabView : View {
-    
-    var body : some View {
-        Text("SECOND TAB VIEW")
-    }
-}
-
-enum Tabs {
-    case FirstTab
-    case SecondTab
-}
 
 // Perhaps this Color extension can go elsewhere
 
 extension Color {
     static let oldPrimaryColor = Color(UIColor.systemOrange)
     static let perfectDog = Color("PerfectDog")
+}
+
+extension UIColor {
+    class var perfectDog: UIColor {
+            let perfectDogColor = 0xF0AC00
+            return UIColor.rgb(fromHex: perfectDogColor)
+        }
+
+        class func rgb(fromHex: Int) -> UIColor {
+
+            let red =   CGFloat((fromHex & 0xFF0000) >> 16) / 0xFF
+            let green = CGFloat((fromHex & 0x00FF00) >> 8) / 0xFF
+            let blue =  CGFloat(fromHex & 0x0000FF) / 0xFF
+            let alpha = CGFloat(1.0)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        }
 }
 
 struct UserPage_Previews: PreviewProvider {
